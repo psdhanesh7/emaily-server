3457584d2ef23796c8d008735f680a9de0bada01
psdhanesh7 <psdhanesh2000@gmail.com>
2021-06-25 21:59:30 +0530

    Ignore dev.js

--- config/dev.js
+++
@@ -1,4 +0,0 @@
-// dev.js - store keys for development environment
-module.exports = {
-    mongoURI: 'mongodb+srv://psdhanesh7:aTwr3XnfTdKp7UCb@cluster0.llxb8.mongodb.net/emaily-dev?retryWrites=true&w=majority'
-}
