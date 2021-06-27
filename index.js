4f715df37bd2da01e55d87b9b7a1621b2bfea1c4
psdhanesh7 <psdhanesh2000@gmail.com>
2021-06-28 00:41:08 +0530

    dfa

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(session({ secret: keys.secret }));
+app.use(session({ 
+    secret: keys.secret,
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(cors());
 app.use(
     cookieSession({
