2971268c55cfc7386363afc38d8e97b018e2df54
nihara75 <niharar25@gmail.com>
2021-06-27 16:28:40 +0530

    changes in production

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://psdhanesh7:aTwr3XnfTdKp7UCb@cluster0.llxb8.mongodb.net/emaily-dev?retryWrites=true&w=majority',
+    JWT_SECRET_TOKEN: 'This is an awesome secret key',
+    client_id:'1032188996382-lh0gi84snssjgc3le812mpa5nfal1fug.apps.googleusercontent.com',
+    secret:'R6nsAgMMxDtzsehVzABJlkLr'
+}
