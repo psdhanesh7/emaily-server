ec36036adace42a689c3df4b59bc7f4d681903d9
nihara75 <niharar25@gmail.com>
2021-06-27 16:32:29 +0530

    changed prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,7 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://psdhanesh7:aTwr3XnfTdKp7UCb@cluster0.llxb8.mongodb.net/emaily-dev?retryWrites=true&w=majority',
-    JWT_SECRET_TOKEN: 'This is an awesome secret key',
-    client_id:'1032188996382-lh0gi84snssjgc3le812mpa5nfal1fug.apps.googleusercontent.com',
-    secret:'R6nsAgMMxDtzsehVzABJlkLr'
+    mongoURI: process.env.mongoURI,
+    JWT_SECRET_TOKEN: process.env.JWT_SECRET_TOKEN,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    client_id: process.env.client_id,
+    secret: process.env.secret
 }
